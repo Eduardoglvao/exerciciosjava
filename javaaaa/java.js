a90201459d6fc8813809291b153e6be0a7fc8240
Eduardo Galvão <127317320+Eduardoglvao@users.noreply.github.com>
2023-08-02 16:37:04 -0300

    Add files via upload

---
+++ javaaaa/java.js
@@ -0,0 +1,60 @@
+//  exercicio para ver se vc e maior de idade ou menor
+
+// let idade = parseInt(prompt('digite sua idade'));
+// if ( idade >= 18 && idade < 30){
+//     console.log('voce tem idade e pode entra')
+
+// }else if ( idade >=30){
+//     console.log( 'voce e velho')
+// }else{
+//     console.log('voce nao atende os requisitos')
+// }
+
+//  exercicio para ver se vc ta de recuperação, aprovado ou reprovado
+// let nota = parseInt(prompt('digite sua nota'));
+// if ( nota >= 7)
+//     console.log('aprovado')
+
+// else if ( nota >=5){
+//     console.log(' recuperção')
+// }else{
+//     console.log('reprovado')
+// }
+
+//  exercicio para dar desconto e calcular.
+// let preço = parseInt(prompt('coloque seu valor'));
+// let valorfinal10 = preço - 10*preço/100;
+// let valorfinal5 = preço - 5*preço/50;
+
+// if( preço >= 100){
+//     alert('você recebe um desconto de 10%')
+// } else if(preço >= 50 )
+// alert('você ganhou um desconto dee 5%')
+// else{
+//     alert('o preço é baixo demais para receber desconto')
+// }
+
+//  para calcular  IMC
+//  peso = parseInt(prompt('coloque seu peso'))
+
+// let altura = parseFloat(prompt('Digite sua altura'));
+
+// let imc = peso / (altura ** 2);
+
+// if (imc <18.5 ) {
+//     alert('magreza.');
+// } else if (imc >= 18.5 && imc <=24.9) {
+//     alert(' normal.');
+// } else if (imc <= 25,0 && img <=29,9) {
+//     alert('sobrepeso.');
+// } else if (imc <= 30,0 && imc <=39,9) {
+//     alert(' obesidade.');
+
+// function converterCelsiusParaFahrenheit(temperaturaCelsius) {
+//  let temperaturaFahrenheit = (temperaturaCelsius * 9/5) + 32;
+//  return temperaturaFahrenheit;
+//  }  let temperaturaCelsius = parseFloat(prompt("Digite a temperatura em graus Celsius:")); 
+// if (!isNaN(temperaturaCelsius)) {
+// let temperaturaFahrenheit = converterCelsiusParaFahrenheit(temperaturaCelsius);
+//  alert("A temperatura em Fahrenheit é: " + temperaturaFahrenheit.toFixed(2));
+// }
